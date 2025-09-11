b26699c8050ba16035601f7c11322765c670ce17
Sri Lakshmi Polavarapu <srilakshmi.polavarapu01@gmail.com>
2025-09-10 18:57:07 -0700

    Updated the code to enhance CLI to use TF-IDF similarity for error matching - changed from extract string matching ---->  similarity based matching

--- mentor.py
+++ mentor.py
@@ -1,21 +1,38 @@
 import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 with open("data/memory.json", "r") as f:
     data = json.load(f)
     
+errors = [entry["error"] for entry in data]
+
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(errors)
 
 user_input = input("Enter the error you are getting : ")    
 
-for entry in data:
-    if entry["error"].lower() in user_input.lower():
-        print("Explanation of the error : ", entry["cause"])
-        print("Here are the possible fixes : ")
-        for s in entry["solutions"]:
+user_vec = vectorizer.transform([user_input])
+
+similarities = cosine_similarity(user_vec, X).flatten()
+
+best_idx = similarities.argmax()
+best_score = similarities[best_idx]
+
+if best_score > 0.2:
+    entry = data[best_idx]
+    print(entry["error"])
+    print(" ---> Explanation of the error : ", entry["cause"])
+    for s in entry["solutions"]:
             print(s)
-        for cmd in entry["next_steps"]:
+    for cmd in entry["next_steps"]:
             print(cmd)
-        print("Here is the suggested YAML : ", entry["snippet"])
-        break   
-    else:
-        print("No match found")
+    print(" ---> Here is the suggested YAML : ", entry["snippet"]) 
+else:
+    print(" ---> No match found")
+
+
+        
+        
 
