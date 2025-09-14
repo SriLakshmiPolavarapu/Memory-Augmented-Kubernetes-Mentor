888e5be14eae57f39db2a1f1c23f58dbae679df1
Sri Lakshmi Polavarapu <srilakshmi.polavarapu01@gmail.com>
2025-09-14 16:09:51 -0700

    trained Naive bayes ML model, evaluated and saved the model

--- train_model.py
+++ train_model.py
@@ -19,3 +19,15 @@ X_vectors = vectorizer.fit_transform(X)
 #Split data
 X_train, X_test, y_train, y_test = train_test_split(X_vectors, y, test_size=0.2, random_state=42)
 
+#Train Naive Bayes
+model = MultinomialNB()
+model.fit(X_train, y_test)
+
+#Evaluate
+y_pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+#Save model and vectorizer
+joblib.dump(model, "models/error_classifier.pkl")
+joblib.dump(vectorizer, "models/tfidf_vectorizer.pkl")
+print("Model and vectorizer saved to 'models/' folder")
